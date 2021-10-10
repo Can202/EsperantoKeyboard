4afdc13873b95019b0c7cc28c63d786d0c0f842a
Can202 <mgoopazo@gmail.com>
2021-10-10 17:52:17 -0300

    better

--- EsperantoLatam.py
+++ EsperantoLatam.py
@@ -3,30 +3,26 @@ import keyboard
 
 def main():
     
-    keyboard.add_hotkey('ctrl+alt+^+j', lambda: write("ĵ"))
-    keyboard.add_hotkey('ctrl+alt+^+c', lambda: write("ĉ"))
-    keyboard.add_hotkey('ctrl+alt+^+g', lambda: write("ĝ"))
-    keyboard.add_hotkey('ctrl+alt+^+h', lambda: write("ĥ"))
-    keyboard.add_hotkey('ctrl+alt+^+s', lambda: write("ŝ"))
-    keyboard.add_hotkey('ctrl+alt+^+h', lambda: write("ĥ"))
-    keyboard.add_hotkey('ctrl+alt+^+u', lambda: write("ŭ"))
+    keyboard.add_hotkey('ctrl+alt+j', lambda: write("ĵ"))
+    keyboard.add_hotkey('ctrl+alt+c', lambda: write("ĉ"))
+    keyboard.add_hotkey('ctrl+alt+g', lambda: write("ĝ"))
+    keyboard.add_hotkey('ctrl+alt+h', lambda: write("ĥ"))
+    keyboard.add_hotkey('ctrl+alt+s', lambda: write("ŝ"))
+    keyboard.add_hotkey('ctrl+alt+h', lambda: write("ĥ"))
+    keyboard.add_hotkey('ctrl+alt+u', lambda: write("ŭ"))
     
-    keyboard.add_hotkey('ctrl+alt+shift+^+j', lambda: write("Ĵ"))
-    keyboard.add_hotkey('ctrl+alt+shift+^+c', lambda: write("Ĉ"))
-    keyboard.add_hotkey('ctrl+alt+shift+^+g', lambda: write("Ĝ"))
-    keyboard.add_hotkey('ctrl+alt+shift+^+h', lambda: write("Ĥ"))
-    keyboard.add_hotkey('ctrl+alt+shift+^+s', lambda: write("Ŝ"))
-    keyboard.add_hotkey('ctrl+alt+shift+^+h', lambda: write("Ĥ"))
-    keyboard.add_hotkey('ctrl+shift+alt+^+u', lambda: write("Ŭ"))
-    keyboard.add_hotkey('alt+shift+s+q', byebye)
+    keyboard.add_hotkey('ctrl+alt+shift+j', lambda: write("Ĵ"))
+    keyboard.add_hotkey('ctrl+alt+shift+c', lambda: write("Ĉ"))
+    keyboard.add_hotkey('ctrl+alt+shift+g', lambda: write("Ĝ"))
+    keyboard.add_hotkey('ctrl+alt+shift+h', lambda: write("Ĥ"))
+    keyboard.add_hotkey('ctrl+alt+shift+s', lambda: write("Ŝ"))
+    keyboard.add_hotkey('ctrl+alt+shift+h', lambda: write("Ĥ"))
+    keyboard.add_hotkey('ctrl+shift+alt+u', lambda: write("Ŭ"))
     while True:
         keyboard.wait()
 def write(string):
     print(string)
     time.sleep(.15)
     keyboard.write(string)
-
-def byebye():
-    quit()
 if __name__ == '__main__':
     main()
